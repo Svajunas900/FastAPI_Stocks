baf1502dc713da53a0335068d7cc495469fa5dc5
Svajunas900 <svajunaskoncius11@gmail.com>
2025-01-10 11:32:12 +0200

    Create two requests each with different url to get stock price and currency price

--- main.py
+++ main.py
@@ -1,9 +1,40 @@
 import requests
 from fastapi import FastAPI
+from dotenv import load_dotenv
+import os
 
-response = requests.get("https://polygon.io/")
+load_dotenv()
+
+POLYGON_KEY = os.getenv("POLYGON_IO_KEY")
+FASTFOREX_KEY = os.getenv("FASTFOREX_KEY")
+
+
+def get_stock_prices_fastforex():
+  url = f"https://api.fastforex.io/fetch-all?api_key={FASTFOREX_KEY}"
+  response = requests.get(url)
+  print(response)
+  print(response.json())
+  return response.json()
+
+
+def get_stock_prices_polygon(stock_ticker, date):
+  date = "2025-01-03"
+  url = f"https://api.polygon.io/v1/open-close/{stock_ticker}/{date}?adjusted=true"
+  headers = {
+    'Authorization': f'Bearer {POLYGON_KEY}'
+  }
+  response = requests.get(url, headers=headers)
+  print(response)
+  print(response.json())
+  return response.json()
+
+
+get_stock_prices_polygon("AAPL","2025-01-03")
+get_stock_prices_fastforex()
+
 
 app = FastAPI()
+
 
 @app.get("/")
 def home():
